da89d5eb4cf2a05b1d17030b247128c045db82fc
Komaly22 <168204372+Komaly22@users.noreply.github.com>
2024-05-24 12:27:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// function number(arr) {
+//     console.log(arr);
+// }
+
+// number([10, 20, 30, 40, 50]);
+
+// function number(arr) {
+//     for (let i = 0; i < arr[i]!=0; i++) {
+//         console.log(arr[i]);
+//     }
+// }
+
+// number([10, 20, 30, 40, 50]);
+
+// function preintNumber(number){
+//     return number;
+// }
+// function passingNumber(num){
+//     return num;
+// }
+// const result = passingNumber(10)*preintNumber(["Tahira",5]);
+// console.log(result);
+
---
+++ index.js
@@ -0,0 +1,47 @@
+// function greet(city){
+//     console.log("i belongs to" + city + "in" + country);
+// } 
+// greet("pune","india");
+// greet('delhi')
+// greet();
+// greet("","india");
+// greet(undrfined,"india");
+// greet("india");
+
+// function age(name,age){
+//     console.log(name + "is a good boy");
+// }
+// age("rajkumar", 20)
+
+// function greet(fname,age){
+//     fname="hi"+fname;
+//     console.log(fname+age);
+// }
+// let fname="gaytri";
+// greet(fname,20);
+
+// function place(city, country){
+//     if(country==undefined){
+//         country='china';
+//     }
+//     console.log("i belong to "+city + " in " + country);
+// }
+// place('delhi');
+
+// function place(city,country){
+//     country=country||'china';
+//     console.log("i belong to " + city + " in "+country);
+// }
+// place('delhi');
+
+function voting(name,age){
+    if (age >=18){
+        console.log("eligible to vote");
+    }
+    else{
+        console.log("not eligible to vote");
+    }
+} 
+voting("Tahira",20)
+
+
